d535f75eade11e55a8bd23067d756b55cf8496f2
ptgyng <2747749134@qq.com>
2026-09-13 20:22:41 +0800

    Update my_strcpy.c

--- my_strcpy.c
+++ my_strcpy.c
@@ -12,3 +12,14 @@ char *my_strcpy(char *dest,const char *src){
 	*dest='\0';
 	return start;
 }
+int my_strlen(const char *str){
+	if(str==NULL){
+		return 0;
+	}
+	int count=0;
+	while(*str!='\0'){
+		count++;
+		str++;
+	}
+	return count;
+}
