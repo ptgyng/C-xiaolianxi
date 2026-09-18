ab7432f0b3a4e98de9ff88101b0e6d316aa5b0b7
ptgyng <2747749134@qq.com>
2026-09-18 20:37:22 +0800

    Update my_strcpy.c

--- my_strcpy.c
+++ my_strcpy.c
@@ -19,3 +19,21 @@ char *my_strcpy(char *dest,const char *src){
 	*dest='\0';
 	return start;
 }
+// 字符串拼接，把src接到dest末尾，dest要有足够空间
+char *my_strcat(char *dest, const char *src)
+{
+	if(dest==NULL||src==NULL){
+		return NULL;
+	}
+	char *start=dest;
+	while(*dest!='\0'){
+		dest++;
+	}
+	while(*src!='\0'){
+		*dest=*src;
+		dest++;
+		src++;
+	}
+	*dest='\0';
+	return start;
+}
